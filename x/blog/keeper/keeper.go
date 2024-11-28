a91391c2c1244152165e25cb0f2e1001217a3bd4
Pablo Ivanov <sosomaso90@gmail.com>
2024-11-29 02:30:36 +0300

    [Blog] Added module

---
+++ x/blog/keeper/keeper.go
@@ -0,0 +1,58 @@
+package keeper
+
+import (
+	"fmt"
+
+	"cosmossdk.io/collections"
+	"cosmossdk.io/core/address"
+	storetypes "cosmossdk.io/core/store"
+	"github.com/cosmos/cosmos-sdk/codec"
+
+	blog "github.com/blog"
+)
+
+type Keeper struct {
+	cdc          codec.BinaryCodec
+	addressCodec address.Codec
+
+	// authority is the address capable of executing a MsgUpdateParams and other authority-gated message.
+	// typically, this should be the x/gov module account.
+	authority string
+
+	// state management
+	Schema      collections.Schema
+	Params      collections.Item[blog.Params]
+	StoredPosts collections.Map[string, blog.StoredPost]
+}
+
+// NewKeeper creates a new Keeper instance
+func NewKeeper(cdc codec.BinaryCodec, addressCodec address.Codec, storeService storetypes.KVStoreService, authority string) Keeper {
+	if _, err := addressCodec.StringToBytes(authority); err != nil {
+		panic(fmt.Errorf("invalid authority address: %w", err))
+	}
+
+	sb := collections.NewSchemaBuilder(storeService)
+	k := Keeper{
+		cdc:          cdc,
+		addressCodec: addressCodec,
+		authority:    authority,
+		Params:       collections.NewItem(sb, blog.ParamsKey, "params", codec.CollValue[blog.Params](cdc)),
+		StoredPosts: collections.NewMap(sb,
+			blog.StoredPostsKey, "storedPosts", collections.StringKey,
+			codec.CollValue[blog.StoredPost](cdc)),
+	}
+
+	schema, err := sb.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	k.Schema = schema
+
+	return k
+}
+
+// GetAuthority returns the module's authority.
+func (k Keeper) GetAuthority() string {
+	return k.authority
+}
